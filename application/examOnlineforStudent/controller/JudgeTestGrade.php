849ca4c4e9c5cf3b21e562d53dd4e51410ef3fe0
Coulson <996714671@qq.com>
2018-11-15 17:22:36 +0800

    第二次

---
+++ application/examOnlineforStudent/controller/JudgeTestGrade.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\examOnlineforStudent\controller;
+
+use think\Controller;
+use think\Db;
+class JudgeTestGrade  extends Controller
+{
+    //提交试卷并批改
+    public function submitTest(){
+    echo"hello";
+
+    }
+
+}
---
+++ application/examOnlineforStudent/model/untest.php
@@ -0,0 +1,9 @@
+<?php 
+namespace app\examOnlineforStudent\model;
+;
+
+use think\Model;
+
+class untest extends Model
+{
+}
