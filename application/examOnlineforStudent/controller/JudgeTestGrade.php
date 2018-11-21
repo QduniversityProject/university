7ef40ba7900c523d124e00b69e0df54a08515992
Coulson <996714671@qq.com>
2018-11-21 08:50:51 +0800

    代码更新

--- application/examOnlineforStudent/controller/JudgeTestGrade.php
+++ application/examOnlineforStudent/controller/JudgeTestGrade.php
@@ -8,22 +8,58 @@ class JudgeTestGrade extends Controller
 {
     //提交试卷并批改
     public function submitTest()
-    
-    {
-        // $value[]=$_GET['multi1'];
-        // // for($i=1;$i<=3;$i++){
-        // // $arr[$i]= $_POST["single{$i}"];
-        // // }
-        // // dump($arr);
-        // print_r ($value);
 
-        
-    $array = $this->request->
-    getParameter("multi[]");   
-    $str =implode(',',$array);   
-    echo $str; 
+    {
+        //单选答案获取
+        // $value[]=$_GET['multi[]'];
+        // for ($i = 1; $i <= 3; $i++) {
+            
+        //     $arr[] = $_GET["single{$i}"];
+        // }
+        // // dump($arr);
+        // print_r($arr);
+
+        // 分割，
+ // $str =implode(',',$array);  
+
+
+          // 多选答案获取
+        // for ($i = 1; $i <= 5; $i++) {
+      
+            // $multi[] = $_GET["multi"];
+        // }
+        // foreach($_GET as $key=>$value){
+        //     echo $key.'='.$vlaue;
+        // } 
+
+
+//      foreach ($_GET as $key => $value[]) 
+// {
+    
+//          echo "Key: $key; Value: $value<br />\n";
+    
+
+// } 
+        print_r($_GET);
+        dump($_GET);
+    // $multi= $_GET;
+    // foreach ($_GET as $val) {
+    //     $val = join(",",$val);
+    //     $temp_array[] = $val;
+    // }
+    // $str =implode('',$temp_array);  
+    // echo($str);
+
+        echo("这是叉开的");
+        foreach($_GET as $val){
+            if(is_array($val)){
+                dump($val);
+            }
+        }
+
+
+
 
 
     }
-
 }
