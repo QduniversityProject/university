b7a53343dccd2c4f6ace2001182695cde3cfad0a
Bill <819542987@qq.com>
2018-11-28 00:01:30 +0800

    按列排序，字段筛选

--- application/examOnlineForTeacher/controller/Mark.php
+++ application/examOnlineForTeacher/controller/Mark.php
@@ -6,8 +6,29 @@ use think\Db;
 
 class Mark extends Controller
 {
-    public function listMark()
+    public function listMark($orderby = 'mark', $orderway = 'desc', $major_id = '',$course_id = '', $tea_id = '', $stu_id = '')
     {
+        $course = Db::table('uncourse')->select();
+        $major = Db::table('unmajor')->select();
+        $teacher = Db::table('unteacher')->select();
+        $student = Db::table('unstudent')->select();
+
+        $where = array();
+        if($major_id){
+            //$where['major_id'] = array('eq', $major_id); 
+            $where['e.major_id'] = $major_id; 
+        }
+        if($course_id){
+            $where['f.course_id'] = $course_id; 
+        }
+        if($tea_id){
+            $where['d.tea_id'] = $tea_id; 
+        }
+        if($stu_id){
+            $where['b.stu_id'] = $stu_id; 
+        }
+
+        
         $markTable=Db::table('unmark')
         ->alias('a')
         ->join('unstudent b','a.stu_id=b.stu_id ')
@@ -15,8 +36,16 @@ class Mark extends Controller
         ->join('unteacher d',' c.tea_id =d.tea_id')
         ->join('unmajor e','b.major_id =e.major_id')
         ->join('uncourse f','c.course_id =f.course_id')
+        ->where($where)
+        ->order("$orderby $orderway")
         ->paginate();
         $this->assign('markTable', $markTable);
+        $this->assign('orderby', $orderby);
+        $this->assign('orderway', $orderway);
+        $this->assign('course', $course);
+        $this->assign('major', $major);
+        $this->assign('teacher', $teacher);
+        $this->assign('student', $student);
         return $this->fetch();
 
     }
