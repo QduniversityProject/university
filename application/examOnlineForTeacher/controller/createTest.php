b3a1a40f89ae80624f4075e3bf8f0d778cf09841
Coulson <996714671@qq.com>
2018-11-28 10:19:18 +0800

    添加考试

--- application/examOnlineForTeacher/controller/createTest.php
+++ application/examOnlineForTeacher/controller/createTest.php
@@ -1,15 +1,13 @@
 <?php
 namespace app\examOnlineForTeacher\controller;
-
-class createTest
+use think\Controller;
+use think\Db;
+class CreateTest extends Controller
 {
     public function createTest()
     {
-        $markTable=Db::query("
-        INSERT INTO `university`.`untest` (`test_id`, `test_desc`, `test_type`, `course_id`, `tea_id`, `single_qus_num`, `multi_qus_num`, `judgment_qus_num`, `blank_qus_num`, `single_score`, `multi_score`, `judgment_score`, `blank_score`, `start_time`, `end_time`) 
-VALUES ('4', '日常练习', '2', '4', '2', '1', '1', '1', '1', '1', '1', '1', '1', '2018-11-23 13:30:00', '2018-11-23 14:00:00');"
-);
-        $this->assign('markTable', $markTable);
+        // $markTable=Db::table('untest');
+        // $this->assign('markTable', $markTable);
         return $this->fetch();
 
     }
