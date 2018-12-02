048606aeb3b6a11e358e6a8edddf6b07db99c26c
Bill <819542987@qq.com>
2018-12-03 03:24:20 +0800

    筛选优化，图表未成功显示

--- application/gradeAnalysis/controller/Analysis.php
+++ application/gradeAnalysis/controller/Analysis.php
@@ -7,9 +7,13 @@ use think\Db;
 
 class Analysis extends Controller
 {
-    public function gradeAnalysis ($course_id = '1', $class_id  = '')
+    public function gradeAnalysis ($course_id = '1', $class_id  = '1', $stu_id = '1')
     {
-        $avg = Db::table('unmark')
+        $class = Db::table('unclass')->select();
+        $course = Db::table('uncourse')->select();
+        $student = Db::table('unstudent')->select();
+
+        $amgrade = Db::table('unmark')
         ->alias('a')
         ->join('unstudent b', 'a.stu_id=b.stu_id ')
         ->join('untest c', 'a.test_id = c.test_id')
@@ -18,12 +22,35 @@ class Analysis extends Controller
         ->join('uncourse f', 'c.course_id =f.course_id')
         ->join('unqbank_type g', 'c.test_type =g.qbank_no')
         ->join('unclass h', 'b.class_id =h.class_id')
-        ->field('b.class_id,avg(a.mark) as avg')
+        ->field('b.class_id, avg(a.mark) as avg, max(a.mark) as max')
         ->where('f.course_id', $course_id)
         ->group("b.class_id")
         ->select();
 
-        $max = Db::table('unmark')
+        // $max = Db::table('unmark')
+        // ->alias('a')
+        // ->join('unstudent b', 'a.stu_id=b.stu_id ')
+        // ->join('untest c', 'a.test_id = c.test_id')
+        // ->join('unteacher d', ' c.tea_id =d.tea_id')
+        // ->join('unmajor e', 'b.major_id =e.major_id')
+        // ->join('uncourse f', 'c.course_id =f.course_id')
+        // ->join('unqbank_type g', 'c.test_type =g.qbank_no')
+        // ->join('unclass h', 'b.class_id =h.class_id')
+        // ->field('b.class_id, max(a.mark) as max')
+        // ->where('f.course_id', $course_id)
+        // ->group("b.class_id")
+        // ->select();
+        
+        foreach ($amgrade as $key => $val){
+            $avg[$key] = $val['avg'];
+            $max[$key] = $val['max'];
+        }
+        // $avg = array($amgrade[0]['avg'],$amgrade[1]['avg']);
+        // halt($avg);
+
+        $where['f.course_id'] = $course_id;
+        $where['b.class_id'] = $class_id;
+        $ccgrade1 = Db::table('unmark')
         ->alias('a')
         ->join('unstudent b', 'a.stu_id=b.stu_id ')
         ->join('untest c', 'a.test_id = c.test_id')
@@ -32,14 +59,75 @@ class Analysis extends Controller
         ->join('uncourse f', 'c.course_id =f.course_id')
         ->join('unqbank_type g', 'c.test_type =g.qbank_no')
         ->join('unclass h', 'b.class_id =h.class_id')
-        ->field('b.class_id, max(a.mark) as max')
-        ->where('f.course_id', $course_id)
-        ->group("b.class_id")
+        ->where($where)
+        ->where('a.mark', '<', '60')
+        ->count();
+
+        $ccgrade2 = Db::table('unmark')
+        ->alias('a')
+        ->join('unstudent b', 'a.stu_id=b.stu_id ')
+        ->join('untest c', 'a.test_id = c.test_id')
+        ->join('unteacher d', ' c.tea_id =d.tea_id')
+        ->join('unmajor e', 'b.major_id =e.major_id')
+        ->join('uncourse f', 'c.course_id =f.course_id')
+        ->join('unqbank_type g', 'c.test_type =g.qbank_no')
+        ->join('unclass h', 'b.class_id =h.class_id')
+        ->where($where)
+        ->where('a.mark', 'between', [60, 70])
+        ->count();
+
+        $ccgrade3 = Db::table('unmark')
+        ->alias('a')
+        ->join('unstudent b', 'a.stu_id=b.stu_id ')
+        ->join('untest c', 'a.test_id = c.test_id')
+        ->join('unteacher d', ' c.tea_id =d.tea_id')
+        ->join('unmajor e', 'b.major_id =e.major_id')
+        ->join('uncourse f', 'c.course_id =f.course_id')
+        ->join('unqbank_type g', 'c.test_type =g.qbank_no')
+        ->join('unclass h', 'b.class_id =h.class_id')
+        ->where($where)
+        ->where('a.mark', '>', '70')
+        ->where('a.mark', '<', '85')
+        ->count();
+
+        $ccgrade4 = Db::table('unmark')
+        ->alias('a')
+        ->join('unstudent b', 'a.stu_id=b.stu_id ')
+        ->join('untest c', 'a.test_id = c.test_id')
+        ->join('unteacher d', ' c.tea_id =d.tea_id')
+        ->join('unmajor e', 'b.major_id =e.major_id')
+        ->join('uncourse f', 'c.course_id =f.course_id')
+        ->join('unqbank_type g', 'c.test_type =g.qbank_no')
+        ->join('unclass h', 'b.class_id =h.class_id')
+        ->where($where)
+        ->where('a.mark', '>=', '85')
+        ->count();
+        // halt($ccgrade1);
+
+        $stugrade = Db::table('unmark')
+        ->alias('a')
+        ->join('unstudent b', 'a.stu_id=b.stu_id ')
+        ->join('untest c', 'a.test_id = c.test_id')
+        ->join('unteacher d', ' c.tea_id =d.tea_id')
+        ->join('unmajor e', 'b.major_id =e.major_id')
+        ->join('uncourse f', 'c.course_id =f.course_id')
+        ->join('unqbank_type g', 'c.test_type =g.qbank_no')
+        ->join('unclass h', 'b.class_id =h.class_id')
+        ->field('h.class_name, b.stu_name, b.stu_rollno, f.course_name, a.mark')
+        ->where('b.stu_id', $stu_id)
         ->select();
-        
-        // halt($max);
+
+
+        $this->assign('class', $class);
+        $this->assign('course', $course);
+        $this->assign('student', $student);
         $this->assign('avg', $avg);
         $this->assign('max', $max);
+        $this->assign('ccgrade1', $ccgrade1);
+        $this->assign('ccgrade2', $ccgrade2);
+        $this->assign('ccgrade3', $ccgrade3);
+        $this->assign('ccgrade4', $ccgrade4);
+        $this->assign('stugrade', $stugrade);
         return $this->fetch();
     }
 }
